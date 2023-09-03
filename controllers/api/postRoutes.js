074b9c70908620d419acd71bf0de9d7f5c1a12e0
Cam <camhack67@gmail.com>
2023-09-03 15:31:52 -0400

    create post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -9,8 +9,10 @@ router.post('/', withAuth, async (req, res) => {
       user_id: req.session.user_id,
     });
 
+    console.log(newPost)
+    
     res.status(200).json(newPost);
-  } catch (err) {
+} catch (err) {
     res.status(400).json(err);
   }
 });
--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
@@ -37,9 +37,11 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/dashboard');
+      console.log("account created")
     } else {
       alert(response.statusText);
+      console.log("account failed")
     }
   }
 };
