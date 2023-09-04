2e5c30a700716224ede8180cc8015f08a97a8a79
Cam <camhack67@gmail.com>
2023-09-04 17:44:57 -0400

    delete posts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -37,5 +37,28 @@ const delButtonHandler = async (event) => {
   }
 };
 
+const showNewPost = async (e) => {
+  if(e.target.matches(".new-post")) {
+  
+    console.log('new post clicked')
+  
+    if (newPostContainer.style.display === 'none') {
+      newPostContainer.style.display = 'flex';
+    } else {
+      newPostContainer.style.display = 'none';
+    }
+  
+  }
+}
+
 const postForm = document.querySelector('.new-post-form')
 postForm.addEventListener('submit', newFormHandler);
+
+const newPostContainer = document.querySelector('.new-post-container');
+const newPostButton = document.querySelector('.new-post');
+newPostButton.addEventListener('click', showNewPost)
+
+const deleteButton = document.querySelectorAll('.previous-buttons')
+deleteButton.forEach(button => {
+  button.addEventListener('click', delButtonHandler);
+});
